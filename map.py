9e027eca5c36c9f356fa8314f2504cd5e1e26735
Ze Hang Zheng <zehang@mit.edu>
2019-04-18 03:50:41 -0400

    Map

---
+++ map.py
@@ -0,0 +1,45 @@
+import sqlite3
+import datetime
+import pickle
+
+
+#monster_location = {"x": [0,2,3], "y": [1,2,4]}  # create a dictionary
+#pickle.dump(monster_location, open("__HOME__/monster.p", "wb"))
+#npc_location = {"x": [1,4,0], "y": [1,3,2]}
+#pickle.dump(npc_location, open("__HOME__/npc.p", "wb"))
+
+class Monster:
+    def __str__(self):
+        return "M"
+
+class NPC:
+    def __str__(self):
+        return "N"
+
+class Map:
+    def __init__(self):
+        self.width = 5
+        self.height = 5
+        self.grid = {(x,y): "&nbsp&nbsp" for x in range(self.width) for y in range(self.height)}
+
+    def __str__(self):
+        map = ""
+        for i in range(self.width):
+            map += "<br/>-------------------------------<br/>|&nbsp&nbsp"
+            for j in range(self.height):
+                map += "" + self.grid[(i,j)].__str__() + "&nbsp&nbsp|&nbsp&nbsp"
+        map += "<br/>-------------------------------<br/>"
+        return map
+
+    def addToMap(self,x, y, npc):
+        self.grid[(x,y)] = npc
+
+def request_handler(request=None):
+    mapper = Map()
+    monster = pickle.load(open( "__HOME__/monster.p", "rb" ))
+    npc = pickle.load(open( "__HOME__/npc.p", "rb" ))
+    for i in range(len(monster["x"])):
+        mapper.addToMap(monster["x"][i], monster["y"][i], Monster())
+    for i in range(len(npc["x"])):
+        mapper.addToMap(npc["x"][i], npc["y"][i], NPC())
+    return mapper.__str__()
