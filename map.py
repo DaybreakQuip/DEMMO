c44b1a7e485bcd49757517742e3097f031f32b5c
Ze Hang Zheng <zehang@mit.edu>
2019-04-18 05:21:05 -0400

    Multiblock

--- map.py
+++ map.py
@@ -10,24 +10,30 @@ class Monster:
 class NPC:
     def __str__(self):
         return "N"
-
+class Player:
+    def __str__(self):
+        return "P"
 class Map:
     def __init__(self):
         self.width = 5
         self.height = 5
-        self.grid = {(x,y): "&nbsp&nbsp" for x in range(self.width) for y in range(self.height)}
+        self.grid = {(x,y): ["&nbsp&nbsp"] for x in range(self.width) for y in range(self.height)}
 
     def __str__(self):
         map = ""
         for i in range(self.width):
             map += "<br/>-------------------------------<br/>|&nbsp&nbsp"
             for j in range(self.height):
-                map += "" + self.grid[(i,j)].__str__() + "&nbsp&nbsp|&nbsp&nbsp"
+                map += "" + [x.__str__() for x in self.grid[(i,j)]].__str__()[1:-1] + "&nbsp&nbsp|&nbsp&nbsp"
         map += "<br/>-------------------------------<br/>"
         return map
 
     def addToMap(self,x, y, npc):
-        self.grid[(x,y)] = npc
+        if self.grid[(x,y)][0] == "&nbsp&nbsp":
+            self.grid[(x,y)] = [npc]
+        else:
+            self.grid[(x,y)].append(npc)
+
 
 def request_handler(request=None):
     mapper = Map()
@@ -37,4 +43,5 @@ def request_handler(request=None):
         mapper.addToMap(monster["x"][i], monster["y"][i], Monster())
     for i in range(len(npc["x"])):
         mapper.addToMap(npc["x"][i], npc["y"][i], NPC())
+    mapper.addToMap(0,1, Player())
     return mapper.__str__()
