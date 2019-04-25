a8cef91e326d5e30c8f543222fae02511acfa93f
Yanni Wang <yanniw@mit.edu>
2019-04-25 06:53:25 -0400

    Add script to reset game database to initial state with default monsters

--- reset_game_database.py
+++ reset_game_database.py
@@ -1,10 +1,12 @@
 """
-This script starts a new game when you run it!
+This script resets the game database to only having default monsters when you run it!
 
 It clears all previous information, so don't run it unless you know what you're doing
 """
 import constants
-from database import Database
+import json
+from containers.monster import Monster
+from database import Database, Serialize
 
 def create_default_monsters():
     """
@@ -12,11 +14,29 @@ def create_default_monsters():
     """
     with open(constants.Database.MONSTER_PATH) as monster_file:
         monster_data = json.load(monster_file)
+
+        monsters = []
+        for monster_info in monster_data['monsters']:
+            id = monster_info['id']
+            row = monster_info['row']
+            col = monster_info['col']
+            health = monster_info['health']
+            power = monster_info['power']
+            is_boss = monster_info['is_boss']
+            defeated_by = set(monster_info['defeated_by'])
+            monsters.append(Monster(id, row, col, health, power, is_boss, defeated_by))
+
+        return monsters
+
 if __name__ == "__main__":
-    # First ensure the database exists
-    Database.create_game_object_tables()
-
-    # Delete any tables if they already exist
+    print("Resetting game database...")
+    # Delete any pre-existing tables to clear any previous monster or player information
     Database.delete_tables()
+    # Now create empty tables in the database
+    Database.create_game_object_tables()
+    # Next, parse the monsters json file to create monster objects
+    monsters = create_default_monsters()
+    # Lastly, add the default monsters to the database
+    Serialize.updateGameObjects(monsters)
+    print("Done!")
 
-    # Populate database with default monsters
