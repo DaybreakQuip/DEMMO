fb3a2f98adf0d1fbe5a3e2f3a01f493821977b3a
Jessica Yang <jyj@mit.edu>
2019-04-24 20:13:05 -0400

    Changed monster table functions

--- database.py
+++ database.py
@@ -21,7 +21,7 @@ def create_tables():
 	conn = sqlite3.connect(game_db)  # connect to that database (will create if it doesn't already exist)
 	c = conn.cursor()  # make cursor into database (allows us to execute commands)
 	c.execute('''CREATE TABLE IF NOT EXISTS player_table (coordinates text, id int, health int, power int, gold int, num_bosses_defeated int);''') # run a CREATE TABLE command
-	c.execute('''CREATE TABLE IF NOT EXISTS monster_table (coordinates text, health int, power int, defeated_by text);''') # run a CREATE TABLE command
+	c.execute('''CREATE TABLE IF NOT EXISTS monster_table (coordinates text, id int, health int, power int, defeated_by text);''') # run a CREATE TABLE command
 	conn.commit() # commit commands
 	conn.close() # close connection to database
 
@@ -60,13 +60,39 @@ def get_player_info(player_id):
 		return info
 	return [info[1]] + [eval(info[0])] + list(info[2:])
 
-def get_monster_info(coordinates):
+def new_monster(monster_id):
+	start_coordinates = (0,0)
+	start_health = 1
+	start_power = 1
 	conn = sqlite3.connect(game_db)  # connect to that database (will create if it doesn't already exist)
 	c = conn.cursor()  # make cursor into database (allows us to execute commands)
-	info = c.execute('''SELECT * FROM monster_table WHERE coordinates = ?;''',(str(coordinates),)).fetchone()
+	c.execute('''INSERT into monster_table VALUES (?,?,?,?,?);''',(str(start_coordinates), monster_id, start_health, start_power, str([]))) # insert row into table
 	conn.commit() # commit commands
 	conn.close() # close connection to database
-	return coordinates
+
+def update_monster(monster_id, coordinates, health, power, defeated_by):
+	conn = sqlite3.connect(game_db)  # connect to that database (will create if it doesn't already exist)
+	c = conn.cursor()  # make cursor into database (allows us to execute commands)
+	c.execute('''UPDATE monster_table SET coordinates = ?, health = ?, power = ?, defeated_by = ? WHERE id = ?;''',(str(coordinates), health, power, str(defeated_by), monster_id))
+	conn.commit() # commit commands
+	conn.close() # close connection to database
+
+def delete_monster(monster_id):
+	conn = sqlite3.connect(game_db)  # connect to that database (will create if it doesn't already exist)
+	c = conn.cursor()  # make cursor into database (allows us to execute commands)
+	c.execute('''DELETE FROM monster_table WHERE id = ?;''',(monster_id,))
+	conn.commit() # commit commands
+	conn.close() # close connection to database
+
+def get_monster_info(monster_id):
+	conn = sqlite3.connect(game_db)  # connect to that database (will create if it doesn't already exist)
+	c = conn.cursor()  # make cursor into database (allows us to execute commands)
+	info = c.execute('''SELECT * FROM monster_table WHERE id = ?;''',(monster_id,)).fetchone()
+	conn.commit() # commit commands
+	conn.close() # close connection to database
+	if info is None:
+		return info
+	return [info[1]] + [eval(info[0])] + list(info[2:4]) + [eval(info[4])]
 
 # dummy player
 player_id = 1
@@ -76,3 +102,12 @@ update_player(player_id, (0,0), 2, 3, 4, 5)
 print(get_player_info(player_id))
 delete_player(player_id)
 print(get_player_info(player_id))
+
+# dummy monster
+monster_id = 10
+new_monster(monster_id)
+print(get_monster_info(monster_id))
+update_monster(monster_id, (1,2), 2, 3, [player_id])
+print(get_monster_info(monster_id))
+delete_monster(monster_id)
+print(get_monster_info(monster_id))
