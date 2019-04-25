0a8907c4a9e1e14d9b4d5d092b2291716715d5e1
Yanni Wang <yanniw@mit.edu>
2019-04-25 06:53:00 -0400

    Add request_handler with basic functionality for GET and POST

---
+++ request_handler.py
@@ -0,0 +1,51 @@
+from database import Database, Serialize, Deserialize
+from containers.player import Player
+from containers.monster import Monster
+
+def handle_get(request):
+    """
+    # TODO: Add more options to this / refine it
+    Handles GET request
+    :param request: the current get request
+    :return: result from GET request
+    """
+    values = request.get('values', {})
+    try:
+        option = values['option']
+        if option== "game_map":
+            game = Deserialize.createGameFromDatabase()
+            return game.get_server_map()
+        elif option == "player":
+            player_id = values['id']
+            player = Database.get_player_info(player_id)
+            return player
+    except:
+        return "Invalid request: {}".format(request)
+
+def handle_post(request):
+    form = request.get('form', {})
+
+    try:
+        # TODO: Change this to player_id & action in the future, for now it is just creating
+        #       a player with that id in that location (row, col)
+        player_id = form['player_id']
+        row = int(form['row'])
+        col = int(form['col'])
+        # Create a new player
+        player = Player(id, row, col)
+        # Add player to database
+        Serialize.updatePlayer(player)
+        # Return information about the player just added
+        return player
+
+    except:
+        return "Invalid request: {}".format(request)
+
+def request_handler(request=None):
+    method = request.get("method")
+    if method == "GET":
+        return handle_get(request)
+    elif method == "POST":
+        return handle_post(request)
+    else:
+        return "Invalid request: {}".format(request)
