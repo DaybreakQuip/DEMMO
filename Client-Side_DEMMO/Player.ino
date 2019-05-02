0c7ed134b54ead8618434d417f5dfc13cc4476e7
Jessica Yang <jyj@mit.edu>
2019-05-02 00:12:46 -0400

    Client map drawn

--- Client-Side_DEMMO/Player.ino
+++ Client-Side_DEMMO/Player.ino
@@ -1,3 +1,4 @@
+/*
 class Player{
   int health;
   int power;
@@ -12,5 +13,14 @@ class Player{
     numBossDefeated = numBoss;
   }
   void drawMap(){
+    const uint16_t BACKGROUND_COLOR = TFT_GREEN;
+    const int SQUARE_SIZE = 4;
+    draw->fillScreen(BACKGROUND_COLOR);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        draw->drawRect(i*5, j*5, SQUARE_SIZE, SQUARE_SIZE, TFT_BLACK);
+      }
+    }
   }
 };
+*/
