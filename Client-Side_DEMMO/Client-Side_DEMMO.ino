a6d519a027f64071554b5a9916f36593bb4443ed
Ze Hang Zheng <zehang@mit.edu>
2019-05-02 00:39:12 -0400

    .ino to .cpp file

--- Client-Side_DEMMO/Client-Side_DEMMO.ino
+++ Client-Side_DEMMO/Client-Side_DEMMO.ino
@@ -4,7 +4,7 @@
 #include <string.h>  //used for some string handling and processing.
 #include <mpu9255_esp32.h>
 #include <HTTPClient.h>
-#include Player.cpp
+#include "Player.cpp"
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 char network[] = "MIT";  //SSID for 6.08 Lab
 String player = "Ze";
@@ -16,82 +16,6 @@ MPU9255 imu; //imu object called, appropriately, imu
 #define MOVE 0 //state of player's action
 #define FIGHT 1 //state of player's action
 int flag = 0;
-
-class Player{
-  public:
-  int health;
-  int power;
-  int gold;
-  int numBossDefeated;
-  TFT_eSPI *draw;
-  Player(TFT_eSPI* tft_to_use, int hp, int atk, int money, int numBoss){
-    draw = tft_to_use;
-    health = hp;
-    power = atk;
-    gold = money;
-    numBossDefeated = numBoss;
-  }
-  void drawMap(String player_map, String flavor_text){
-    const uint16_t BACKGROUND_COLOR = TFT_BLACK;
-    const uint16_t MAP_OUTLINE_COLOR = TFT_WHITE;
-    const uint16_t PLAYER_COLOR = TFT_GREEN;
-    const uint16_t MONSTER_COLOR = TFT_RED;
-    const int SQUARE_SIZE = 15;
-    draw->fillScreen(BACKGROUND_COLOR);
-
-    // draw player map
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        draw->drawRect(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, MAP_OUTLINE_COLOR);
-
-        // draw monster
-        int start = (j*3 + i)*3;
-        if (player_map.charAt(start) == 'M') {
-          draw->fillRect(i*SQUARE_SIZE + 1, j*SQUARE_SIZE + 1, SQUARE_SIZE - 2, SQUARE_SIZE - 2, MONSTER_COLOR);
-        }
-
-        // draw player
-        if (player_map.charAt(start + 1) == 'P') {
-          draw->fillCircle(i*SQUARE_SIZE + SQUARE_SIZE/2, j*SQUARE_SIZE + SQUARE_SIZE/2, 2, PLAYER_COLOR);
-        }
-      }
-    }
-
-    // draw player stats
-    const int text_x = 3*SQUARE_SIZE + 10;
-    for (int i = 0; i < 5; i++) {
-      draw->setCursor(text_x, i*10);
-      if (i == 0) {
-        draw->println("   Stats   ");
-      } else if (i == 1) {
-        draw->println("HP:   " + String(health));
-      } else if (i == 2) {
-        draw->println("PWR:  " + String(power));
-      } else if (i == 3) {
-        draw->println("Gold: " + String(gold));
-      } else if (i == 4) {
-        draw->println("Boss: " + String(numBossDefeated));
-      } 
-    }
-
-    // draw player options
-    const int options_y = 3*SQUARE_SIZE + 10;
-    const uint16_t OPTIONS_COLOR = TFT_YELLOW;
-    draw->drawRect(10, options_y, 105, 20, OPTIONS_COLOR);
-    draw->setCursor(30, options_y + 6);
-    draw->println("BUY    QUIT");
-    // cursor options for later
-    //draw->setCursor(20, options_y + 6);
-    //draw->println(">");
-    //draw->setCursor(60, options_y + 6);
-    //draw->println(">");
-
-    // draw flavor text
-    const int flavor_y = options_y + 30;
-    draw->setCursor(0, flavor_y);
-    draw->println(flavor_text);
-  }
-};
 
 Player ze(&tft, 11, 5, 3, 10);
 
@@ -170,19 +94,17 @@ void action(){
       break;
     }
 }
-void post_request(String action){
+String post_request(String action){
   HTTPClient http;
   http.begin("http://608dev.net/sandbox/sc/zehang/DEMMO/request_handler.py");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded"); 
   int httpResponseCode = http.POST("player_id=" + player + "&action=" + action);
   if(httpResponseCode>0){
     String response = http.getString();
-    Serial.println(httpResponseCode);
-    Serial.println(response);
+    return response;
   }
   else{
-    Serial.print("Error on sending POST: ");
-    Serial.println(httpResponseCode);
+    return "Error on sending POST: ";
   }
   http.end();
 }
--- Client-Side_DEMMO/Player.cpp
+++ Client-Side_DEMMO/Player.cpp
@@ -1,4 +1,8 @@
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <SPI.h> //Used in support of TFT Display
+#include <string.h>  //used for some string handling and processing.
 class Player{
+  public:
   int health;
   int power;
   int gold;
@@ -11,14 +15,64 @@ class Player{
     gold = money;
     numBossDefeated = numBoss;
   }
-  void drawMap(){
-    const uint16_t BACKGROUND_COLOR = TFT_GREEN;
-    const int SQUARE_SIZE = 4;
+  void drawMap(String player_map, String flavor_text){
+    const uint16_t BACKGROUND_COLOR = TFT_BLACK;
+    const uint16_t MAP_OUTLINE_COLOR = TFT_WHITE;
+    const uint16_t PLAYER_COLOR = TFT_GREEN;
+    const uint16_t MONSTER_COLOR = TFT_RED;
+    const int SQUARE_SIZE = 15;
     draw->fillScreen(BACKGROUND_COLOR);
+
+    // draw player map
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        draw->drawRect(i*5, j*5, SQUARE_SIZE, SQUARE_SIZE, TFT_BLACK);
+        draw->drawRect(i*SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, MAP_OUTLINE_COLOR);
+
+        // draw monster
+        int start = (j*3 + i)*3;
+        if (player_map.charAt(start) == 'M') {
+          draw->fillRect(i*SQUARE_SIZE + 1, j*SQUARE_SIZE + 1, SQUARE_SIZE - 2, SQUARE_SIZE - 2, MONSTER_COLOR);
+        }
+
+        // draw player
+        if (player_map.charAt(start + 1) == 'P') {
+          draw->fillCircle(i*SQUARE_SIZE + SQUARE_SIZE/2, j*SQUARE_SIZE + SQUARE_SIZE/2, 2, PLAYER_COLOR);
+        }
       }
     }
+
+    // draw player stats
+    const int text_x = 3*SQUARE_SIZE + 10;
+    for (int i = 0; i < 5; i++) {
+      draw->setCursor(text_x, i*10);
+      if (i == 0) {
+        draw->println("   Stats   ");
+      } else if (i == 1) {
+        draw->println("HP:   " + String(health));
+      } else if (i == 2) {
+        draw->println("PWR:  " + String(power));
+      } else if (i == 3) {
+        draw->println("Gold: " + String(gold));
+      } else if (i == 4) {
+        draw->println("Boss: " + String(numBossDefeated));
+      } 
+    }
+
+    // draw player options
+    const int options_y = 3*SQUARE_SIZE + 10;
+    const uint16_t OPTIONS_COLOR = TFT_YELLOW;
+    draw->drawRect(10, options_y, 105, 20, OPTIONS_COLOR);
+    draw->setCursor(30, options_y + 6);
+    draw->println("BUY    QUIT");
+    // cursor options for later
+    //draw->setCursor(20, options_y + 6);
+    //draw->println(">");
+    //draw->setCursor(60, options_y + 6);
+    //draw->println(">");
+
+    // draw flavor text
+    const int flavor_y = options_y + 30;
+    draw->setCursor(0, flavor_y);
+    draw->println(flavor_text);
   }
 };
