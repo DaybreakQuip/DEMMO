a9c7867cc4df479b4163a737390ee16da9c53296
Ze Hang Zheng <zehang@mit.edu>
2019-05-10 01:45:45 -0400

    post request back to client side

--- Client-Side_DEMMO/Client-Side_DEMMO.ino
+++ Client-Side_DEMMO/Client-Side_DEMMO.ino
@@ -145,3 +145,37 @@ string action(){
   }
         
 }
+
+string post_request(string player, string action){
+  //Note to self, to convert integer to string: string boss = "Boss: " + string(itoa(numBossDefeated, buffer, 10));
+  WiFiClient client;
+  string body = "player_id=" + player + "&action=" + action;
+  if (client.connect("608dev.net", 80)) {
+    client.println("POST http://608dev.net/sandbox/sc/zehang/DEMMO/request_handler.py HTTP/1.1");
+    client.println("Host: 608dev.net");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(body.length());
+    client.println();
+    client.println(body.c_str());
+    //Serial.println(body.c_str());
+
+  }
+  string buff = "";
+  buff = "  ";
+  string response = "";
+  bool canRespond = false;
+  while(!client.available()){}
+   while ( client.available())
+    {
+      char resp = client.read();
+      buff += resp;
+      if (canRespond){
+        response += resp;
+      }
+      if (buff.substr(buff.length()-2, buff.length()) == "\n\r"){
+        canRespond = true;
+      }
+    }
+  return response.substr(1, response.length());
+}
